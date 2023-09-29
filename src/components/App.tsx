292337d58fb44a1d22889425fa2ebb43b99db41b
DennisNikolay <dennisnikolay@posteo.de>
2023-09-29 17:35:28 +0200

    build: add matrix sdk

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,15 @@
+import * as sdk from "matrix-js-sdk";
+import {useEffect, useMemo} from "react";
+
 export function App() {
+  const matrixClient = useMemo(
+    () => sdk.createClient({baseUrl: "http://synapse.local"}),
+    [],
+  );
+
+  useEffect(() => {
+    matrixClient.startClient({initialSyncLimit: 10});
+  }, [matrixClient]);
+
   return <div>Hello World!</div>;
 }
