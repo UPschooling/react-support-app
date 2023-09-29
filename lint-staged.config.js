d4032ca2a63a9a36fd866acdfac3e8a8251b04c3
DennisNikolay <dennisnikolay@posteo.de>
2023-09-29 12:44:02 +0200

    build: run related unit tests on commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   "*": (filenames) => `pnpm ls-lint ${filenames.join(" ")}`,
 
-  "**/*.(ts|tsx)": () => "pnpm tsc --noEmit",
+  "**/*.(ts|tsx)": () => ["pnpm tsc --noEmit"],
 
   "**/*.(ts|tsx|js)": (filenames) => [
     `pnpm eslint --fix ${filenames.join(" ")}`,
     `pnpm prettier --write ${filenames.join(" ")}`,
+    `vitest related --run  ${filenames.join(" ")}`,
   ],
 
   "**/*.(md|json)": (filenames) =>
